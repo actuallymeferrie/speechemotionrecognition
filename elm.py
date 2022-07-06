5e0eade80756d67b9b613eaa02c004f9a69c0cee
actuallymeferrie <fersarz30@gmail.com>
2022-07-06 16:05:34 +0800

    Update elm.py

--- elm.py
+++ elm.py
@@ -1,6 +1,13 @@
 from setup import *
 
-
-
+audio_file = open('YAF_back_angry.wav', 'rb')
+audio_bytes = audio_file.read()
+st.audio(audio_bytes, format='audio/wav')
 
 dscnn_model = tf.keras.models.load_model('Models/500e.h5')
+layer_name = 'flatten'
+
+input = dscnn_model.input
+output = dscnn_model.get_layer(layer_name).output
+
+hidden_layer_model = Model(inputs = input  , outputs = output )
