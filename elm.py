c5d5977cb5fbe65edb4259657be955210c52d901
actuallymeferrie <fersarz30@gmail.com>
2022-07-06 18:09:20 +0800

    Update elm.py

--- elm.py
+++ elm.py
@@ -16,11 +16,13 @@ layer_name = 'flatten'
 input = dscnn_model.input
 output = dscnn_model.get_layer(layer_name).output
 
+st.write("Input",input)
+st.write("Output",output)
 hidden_layer_model = Model(inputs = input  , outputs = output )
 dscnn_train_result = hidden_layer_model.predict(image_input)
 #elm
-elm_model = hpelm.elm.ELM(dscnn_train_result.shape[1] , NUM_CLASS)
-elm_model.add_neurons( ELM_HIDDEN_NEURONS , func = 'sigm')
-elm_model.train(dscnn_train_result, target_train_oh, 'ml')
-dscnn_elm_result = hidden_layer_model.predict(test_image_batch)
-predictions = elm_model.predict(dscnn_elm_result)
+# elm_model = hpelm.elm.ELM(dscnn_train_result.shape[1] , NUM_CLASS)
+# elm_model.add_neurons( ELM_HIDDEN_NEURONS , func = 'sigm')
+
+# dscnn_elm_result = hidden_layer_model.predict(test_image_batch)
+# predictions = elm_model.predict(dscnn_elm_result)
